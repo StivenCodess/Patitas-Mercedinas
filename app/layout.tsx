41316290dfde7250869824e1c69fd427b35dae81
Tiven <vairtor@gmail.com>
2024-01-14 17:15:32 -0300

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ export const metadata: Metadata = {
 	title: "Patitas Mercedinas",
 	description:
 		"Descubre la ternura en cada rincón de Villa Mercedes con Patitas Mercedinas. Encuentra y comparte historias de perritos callejeros en busca de amor y un hogar. Únete a nuestra comunidad dedicada a ayudar a estos peludos amigos a encontrar la felicidad que se merecen. ¡Adopta, comparte y haz la diferencia con Patitas Mercedinas!",
+	icons: {
+		icon: ["/icon/favicon.ico?v=4"],
+		apple: ["/icon/apple-touch-icon.png?v=4"],
+		shortcut: ["/icon/apple-touch-icon.png"],
+	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
