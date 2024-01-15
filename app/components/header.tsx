e69b0d1a11ef325f002ff966a684fd2b876d1768
Tiven <vairtor@gmail.com>
2024-01-15 12:57:05 -0300

    Add header component

---
+++ app/components/header.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+import Blob1 from "./icons/Blob1";
+import Blob2 from "./icons/Blob2";
+import DogTracks from "./icons/DogTracks";
+
+import localFont from "next/font/local";
+const mangoFont = localFont({ src: "./../../public/font/Mango-Regular.woff2" });
+
+const aristotelica = localFont({
+	src: "./../../public/font/AristotelicaProText-Light.woff2",
+});
+
+const Header = () => {
+	return (
+		<header className="bg-white w-full h-80 relative overflow-hidden flex justify-center sm:items-center flex-col items-center ">
+			<section className="text-center flex flex-col sm:gap-3 mb-10 max-w-[500px] ml-5 w-96 items-center ">
+				<div className="w-10 flex items-center justify-center h-10 bg-[#5CE1E6] fill-white rounded-full sm:mb-2 mb-5">
+					<DogTracks className="w-5 rounded-full" />
+				</div>
+				<span
+					className={`${aristotelica.className} uppercase text-black  sm:text-xl sm:w-[400px] text-md tracking-[0.23em] z-30`}
+				>
+					Welcome To
+				</span>
+				<h1
+					className={`${mangoFont.className} sm:text-6xl text-5xl sm:w-[400px] uppercase text-[#5CE1E6] z-30  `}
+				>
+					Patitas Mercedinas
+				</h1>
+			</section>
+			<Blob1 className="absolute w-80 -top-64 -left-24 rotate-[36deg] opacity-50"></Blob1>
+			<Blob2 className="absolute w-80 -bottom-32 -right-20  sm:-left-10 rotate-[-171deg] opacity-50" />
+			<Image
+				src="/perroHeader.png"
+				width={370}
+				height={500}
+				className="absolute right-0 bottom-1 max-sm:w-80 sm:block hidden"
+				alt="Perro portada"
+			/>
+		</header>
+	);
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,10 @@
-import Image from "next/image";
-
-import Blob1 from "./components/icons/Blob1";
-import Blob2 from "./components/icons/Blob2";
-import DogTracks from "./components/icons/DogTracks";
 import AnimalCard from "./components/AnimalCard";
-
-import localFont from "next/font/local";
-const mangoFont = localFont({ src: "./../public/font/Mango-Regular.woff2" });
-
-const aristotelica = localFont({
-	src: "./../public/font/AristotelicaProText-Extralight.woff2",
-});
+import Header from "./components/header";
 
 export default function Home() {
 	return (
 		<>
-			<header className="bg-white w-full h-80 relative overflow-hidden flex justify-center sm:items-center flex-col items-center ">
-				<section className="text-center flex flex-col sm:gap-3 mb-10 max-w-[500px] ml-5 w-96 items-center ">
-					<div className="w-10 flex items-center justify-center h-10 bg-[#5CE1E6] fill-white rounded-full sm:mb-2 mb-5">
-						<DogTracks className="w-5 rounded-full" />
-					</div>
-					<span
-						className={`${aristotelica.className} uppercase text-black  sm:text-xl sm:w-[400px] text-md tracking-[0.23em] z-30`}
-					>
-						Welcome To
-					</span>
-					<h1
-						className={`${mangoFont.className} sm:text-6xl text-5xl sm:w-[400px] uppercase text-[#5CE1E6] z-30  `}
-					>
-						Patitas Mercedinas
-					</h1>
-				</section>
-				<Blob1 className="absolute w-80 -top-64 -left-24 rotate-[36deg] opacity-50"></Blob1>
-				<Blob2 className="absolute w-80 -bottom-32 -right-20  sm:-left-10 rotate-[-171deg] opacity-50" />
-				<Image
-					src="/perroHeader.png"
-					width={370}
-					height={500}
-					className="absolute right-0 bottom-1 max-sm:w-80 sm:block hidden"
-					alt="Perro portada"
-				/>
-			</header>
+			<Header></Header>
 			{/* TODO: ARREGLAR LOS TAMAÑOS DE LOS ANIMALITOS */}
 			<main className="flex max-w-5xl gap-5 gap-x-8 sm:flex-wrap flex-col sm:flex-row justify-center items-center -mt-5 z-40">
 				<AnimalCard
@@ -75,6 +39,11 @@ export default function Home() {
 					color="bg-[#C982D3]"
 				/>
 			</main>
+			<footer className=" w-full flex justify-center flex-col items-center mt-20 p-3">
+				<button className="bg-[#12a9e9] px-10 py-3 uppercase rounded-md text-white flex justify-center items-center text-2xl gap-2">
+					Donar
+				</button>
+			</footer>
 		</>
 	);
 }
