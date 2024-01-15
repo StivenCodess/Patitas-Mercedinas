0eb17ee938bca122f68476ac1224088ce09213fa
Tiven <vairtor@gmail.com>
2024-01-15 12:57:27 -0300

    Refacotor page

---
+++ app/donation/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/header";
+
+export default function Home() {
+	return (
+		<>
+			<Header />
+			<p>
+				¡Gracias por tu generosa intención de donar! Tu nombre será reconocido en nuestra
+				tabla de donadores. Tus contribuciones se destinan directamente a proporcionar
+				alimentos y cuidados esenciales para perros y gatos necesitados. ¡Gracias por ser
+				parte de nuestro compromiso con el bienestar animal!
+			</p>
+		</>
+	);
+}
