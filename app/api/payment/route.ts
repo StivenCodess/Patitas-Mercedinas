9207be45261f52f9049109ccd9957dbe1f57fdc7
Tiven <vairtor@gmail.com>
2024-01-19 11:44:40 -0300

    Add save payments on supabase

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { MercadoPagoConfig, Payment } from "mercadopago";
+import { createClient } from '@supabase/supabase-js'
 
 const client = new MercadoPagoConfig({
 	accessToken: process.env.MP_ACCESS_TOKEN as string,
 });
+
+const supabase = createClient(process.env.SUPABASE_URL! , process.env.SUPABASE_SECRET!)
 
 export async function POST(request: NextRequest) {
     const body = await request.json().then(data => data as {data: {id: string}})
@@ -13,6 +16,14 @@ export async function POST(request: NextRequest) {
     console.log("BODY -> ",body)
     console.log("PAYMENT -> ",payment)
 
+    const donation = {
+        id: payment.id,
+        name: payment.payer?.first_name,
+        amount: payment.transaction_amount,
+        message: payment.description
+    }
+
+    const supabaseResult = await supabase.from("donations").insert(donation)
+
     return NextResponse.json({success: true})
 }
-
